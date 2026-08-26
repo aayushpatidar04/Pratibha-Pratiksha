467f497812e539ce71b653e407e9c49f39a86085
Aayush Patidar <aayushpatidar04@gmail.com>
2026-08-26 13:26:20 +0530

    drop building also commented

--- database/migrations/2026_08_26_120625_remove_building_id_from_mess_menu_table.php
+++ database/migrations/2026_08_26_120625_remove_building_id_from_mess_menu_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::table('mess_menu', function (Blueprint $table) {
             // $table->dropUnique('mess_menu_unique_building_day_meal');
-            $table->dropColumn('building_id');
+            // $table->dropColumn('building_id');
             $table->json('items')->change();
             $table->unique(
                 ['menu_date', 'meal_type'],
