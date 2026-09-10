87715450f291aaf8cda700a66372bd87d1448303
Aayush Patidar <aayushpatidar04@gmail.com>
2026-09-10 11:32:10 +0530

    build

--- public/build/assets/chevron-left-OKQ_X4EJ.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./createLucideIcon-cVavRW4w.js";var t=e(`chevron-left`,[[`path`,{d:`m15 18-6-6 6-6`,key:`1wnfg3`}]]);export{t};
--- public/build/assets/chevron-right-CNwrFkHQ.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./createLucideIcon-cVavRW4w.js";var t=e(`chevron-right`,[[`path`,{d:`m9 18 6-6-6-6`,key:`mthhwq`}]]);export{t};
